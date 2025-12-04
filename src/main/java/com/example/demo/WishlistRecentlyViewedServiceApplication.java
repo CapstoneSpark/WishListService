90f8b61ef11ab131191177ad7e2c81f7cfbcd9dd
LXXPK <reddykiran9611@gmail.com>
2025-12-04 12:52:55 +0530

    after demo

--- src/main/java/com/example/demo/WishlistRecentlyViewedServiceApplication.java
+++ src/main/java/com/example/demo/WishlistRecentlyViewedServiceApplication.java
@@ -3,15 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class WishlistRecentlyViewedServiceApplication {
 
     public static void main(String[] args) {
-    	  Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-          System.setProperty("DB_USER", dotenv.get("DB_USER"));
-          System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+
         SpringApplication.run(WishlistRecentlyViewedServiceApplication.class, args);
     }
 }
